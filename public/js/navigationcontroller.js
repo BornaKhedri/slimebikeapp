7d9c0b7ff2f9b12186f443ec40137ecd488e56b9
Chintan Pathak <cp84@uw.edu>
2020-05-27 21:41:46 +0000

    fix to ms_ids repeatition on identification-back click

--- public/js/navigationcontroller.js
+++ public/js/navigationcontroller.js
@@ -37,8 +37,9 @@ $(function () {
     // This function is called when continue button on the "location" is clicked
     $('#classification_continue').click(function (e) {
         e.preventDefault();
-        // reset the infraction ids array, as it gets refilled when we continue
+        // reset the infraction ids array as well as ms_ids, as it gets refilled when we continue
         infraction_ids = []
+        micromobilityservice_ids = [];
         var next_tab = $('a[href="#tab_identification"]');
         var $infractions = $( "input[name*='infraction']:checked");
         var $companies = $("input[name*='company']:checked");
